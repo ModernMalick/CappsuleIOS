daa1cc1954da4aaea6579bd02710a9c4bbfd44b5
Malick NDIAYE <malick1ndiaye0@gmail.com>
2022-01-03 19:26:15 +0100

    Home Button Design Done

--- Shared/SubViews/Home/HomeView.swift
+++ Shared/SubViews/Home/HomeView.swift
@@ -30,113 +30,118 @@ struct HomeView: View {
 	}
 	
     var body: some View {
-		VStack{
-			if(locationManager.statusString == "authorizedWhenInUse" || locationManager.statusString == "authorizedAlways"){
-				HStack(){
-					Image("location")
-						.renderingMode(.template)
-						.foregroundColor(Color("ColorOnBackground"))
-					Text(city)
-						.frame(width: 90.0)
+		ZStack{
+			VStack{
+				if(locationManager.statusString == "authorizedWhenInUse" || locationManager.statusString == "authorizedAlways"){
+					HStack(){
+						Image("location")
+							.renderingMode(.template)
+							.foregroundColor(Color("ColorOnBackground"))
+						Text(city)
+							.frame(width: 90.0)
+						Spacer()
+						Image(weatherlogo)
+							.resizable()
+							.frame(width: 60.0, height: 60.0)
+						Spacer()
+						Text(temperature + "°" + unit)
+							.frame(width: 90.0, alignment: .trailing)
+						Image("thermometer")
+							.renderingMode(.template)
+							.foregroundColor(Color("ColorOnBackground"))
+					}.onAppear(perform: getWeather)
+						.padding(.horizontal, 10.0)
 					Spacer()
-					Image(weatherlogo)
-						.resizable()
-						.frame(width: 60.0, height: 60.0)
+				}
+				HStack{
+					Text("Light")
+						.fontWeight(selectedWarmthHeavy ? .regular : .bold)
+						.frame(width: 55.0)
 					Spacer()
-					Text(temperature + "°" + unit)
-						.frame(width: 90.0, alignment: .trailing)
-					Image("thermometer")
-						.renderingMode(.template)
-						.foregroundColor(Color("ColorOnBackground"))
-				}.onAppear(perform: getWeather)
-				.padding(.horizontal, 10.0)
+						.frame(width: 50.0)
+					Toggle("", isOn: Binding<Bool>(
+						get: {selectedWarmthHeavy},
+						set: {
+							selectedWarmthHeavy = $0
+							setWeather()
+						}))
+						.tint(Color("AccentLight"))
+						.labelsHidden()
+					Spacer()
+						.frame(width: 50.0)
+					Text("Heavy")
+						.fontWeight(selectedWarmthHeavy ? .bold : .regular)
+						.frame(width: 55.0)
+				}
+				Spacer()
+				HomeOutfitView(selectedWarmth: selectedWarmthFilter)
 				Spacer()
 			}
-			HStack{
-				Text("Light")
-					.fontWeight(selectedWarmthHeavy ? .regular : .bold)
-					.frame(width: 55.0)
-				Spacer()
-					.frame(width: 50.0)
-				Toggle("", isOn: Binding<Bool>(
-					get: {selectedWarmthHeavy},
-					set: {
-						selectedWarmthHeavy = $0
-						setWeather()
-					}))
-					.tint(Color("AccentLight"))
-				.labelsHidden()
-				Spacer()
-					.frame(width: 50.0)
-				Text("Heavy")
-					.fontWeight(selectedWarmthHeavy ? .bold : .regular)
-					.frame(width: 55.0)
-			}
-			Spacer()
-			HomeOutfitView(selectedWarmth: selectedWarmthFilter)
-			Spacer()
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
 			HStack{
 				Spacer()
-				if(!showMore){
-					Button(action: {
-						showMore.toggle()
-					}){
-						Image(systemName: "camera")
-							.scaleEffect(1.65)
-							.tint(Color.black)
-					}
-					.frame(width: 65, height: 65)
-					.background(Color("AccentLight"))
-					.cornerRadius(50)
-				} else {
-					VStack{
+				VStack{
+					Spacer()
+					if(!showMore){
 						Button(action: {
 							showMore.toggle()
 						}){
-							Image(systemName: "square.and.arrow.up")
-								.scaleEffect(1.45)
-								.tint(Color.black)
-						}
-						.frame(width: 45, height: 45)
-						.background(Color("AccentLight"))
-						.cornerRadius(50)
-						Button(action: {
-							showMore.toggle()
-						}){
-							Image(systemName: "heart")
-								.scaleEffect(1.45)
-								.tint(Color.black)
-						}
-						.frame(width: 45, height: 45)
-						.background(Color("AccentLight"))
-						.cornerRadius(50)
-						Button(action: {
-							print("refresh")
-						}){
-							Image(systemName: "arrow.2.circlepath")
-								.scaleEffect(1.45)
-								.tint(Color.black)
-						}
-						.frame(width: 45, height: 45)
-						.background(Color("AccentLight"))
-						.cornerRadius(50)
-						Button(action: {
-							showMore.toggle()
-						}){
-							Image(systemName: "multiply")
-								.scaleEffect(2)
+							Image(systemName: "camera")
+								.scaleEffect(1.65)
 								.tint(Color.black)
 						}
 						.frame(width: 65, height: 65)
 						.background(Color("AccentLight"))
 						.cornerRadius(50)
+					} else {
+						VStack{
+							Button(action: {
+								showMore.toggle()
+							}){
+								Image(systemName: "square.and.arrow.up")
+									.scaleEffect(1.45)
+									.tint(Color.black)
+							}
+							.frame(width: 45, height: 45)
+							.background(Color("AccentLight"))
+							.cornerRadius(50)
+							Button(action: {
+								showMore.toggle()
+							}){
+								Image(systemName: "heart")
+									.scaleEffect(1.45)
+									.tint(Color.black)
+							}
+							.frame(width: 45, height: 45)
+							.background(Color("AccentLight"))
+							.cornerRadius(50)
+							Button(action: {
+								showMore.toggle()
+							}){
+								Image(systemName: "arrow.2.circlepath")
+									.scaleEffect(1.45)
+									.tint(Color.black)
+							}
+							.frame(width: 45, height: 45)
+							.background(Color("AccentLight"))
+							.cornerRadius(50)
+							Button(action: {
+								showMore.toggle()
+							}){
+								Image(systemName: "multiply")
+									.scaleEffect(2)
+									.tint(Color.black)
+							}
+							.frame(width: 65, height: 65)
+							.background(Color("AccentLight"))
+							.cornerRadius(50)
+						}
 					}
 				}
 			}
-			.padding(.trailing, 10.0)
 			.frame(maxWidth: .infinity)
 		}
-		.frame(maxWidth: .infinity, maxHeight: .infinity)
+		.padding(.all, 20.0)
 	}
 	func getWeather(){
 		locationManager.geocoder.reverseGeocodeLocation(locationManager.lastLocation!, completionHandler:
